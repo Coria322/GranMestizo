6273ee5bc9bf5a186bed9b999e47b8bb5a0dfa2e
Coria_32 <coriayo32@gmail.com>
2025-05-17 11:35:25 -0600

    Ajustando vistas de prueba

--- resources/views/Admin/main.blade.php
+++ resources/views/Admin/main.blade.php
@@ -1,15 +1,24 @@
-
 <div>
     <h1 style="color:rebeccapurple;">ADMIN</h1>
-    <h1>ya estas logeado</h1>
+    <h1>Ya estás logeado</h1>
     <h2>Bienvenido {{ $usuario->USUARIO_NOMBRE }}</h2>
-    <h2>Tu APELLIDO es {{ $usuario->USUARIO_APELLIDO }}</h2>
+    <h2>Tu apellido es {{ $usuario->USUARIO_APELLIDO }}</h2>
     <h2>Tu correo es {{ $usuario->USUARIO_CORREO }}</h2>
     <h2>Tu contraseña es {{ $usuario->USUARIO_PWD }}</h2>
     <h2>Tu rol es {{ $usuario->USUARIO_ROL }}</h2>
-    <h2>Tu id es {{ $usuario->USUARIO_ID }}</h2>
+    <h2>Tu ID es {{ $usuario->USUARIO_ID }}</h2>
+
+    <hr>
+
+    <h1>Datos del ADMIN</h1>
+    @foreach ($usuario->admin->getAttributes() as $key => $value)
+        <h2>{{ ucfirst(str_replace('_', ' ', $key)) }}: {{ $value }}</h2>
+    @endforeach
+
+    <hr>
+
     <form action="{{ route('logout') }}" method="post">
         @csrf
         <button type="submit">Cerrar sesión</button>
-        
+    </form>
 </div>
