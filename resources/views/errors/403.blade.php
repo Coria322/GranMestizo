797cf6fcd572092c3a0d2ed3c257a8a48b92bf6d
Coria_32 <coriayo32@gmail.com>
2025-05-18 11:06:49 -0600

    Corrigiendo mensajes de error 403

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -4,14 +4,7 @@
 @section('num2', '3')
 @section('error', 'Forbidden')
 @section('content')
-@if(session('error'))
-    <div class="msg">{{ session('error') }}</div>
-@endif
-
-@if(session('Error'))
-    <div class="msg">{{ session('error') }}</div>
-@endif
-
+<div class="msg">{{ $exception->getMessage() }}</div>
 @endsection
 
 
